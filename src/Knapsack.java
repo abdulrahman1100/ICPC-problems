67e7def6587aeb2348931fd3b1aab7a4ae3062fb
Abdoh <47214336+abdulrahman1100@users.noreply.github.com>
2020-11-16 21:57:30 +0200

    Update Knapsack.java

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,72 +1,69 @@
-import java.util.*;
-import java.io.*;
-public class Knapsack {
+package Specialists;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class knapsack2 {
 	
-	static int []w,v;
-	static int n,s;
-	static int [][]dp;
-	public static int dp(int index,int remw) {
+	static int [] weights,values;
+	static int n,w;
+	static long [][]dp;
+	public static  long knapsack(int index,int w) {
 		
-		if(remw<0) 
+		if(w<0) {
 			return -(int)1e9;
-		
-		if(index==n) 
-			return 0;
-		
-		
-		
-		if(dp[index][remw]!=-1) 
-			return dp[index][remw];
-		
-		int take=v[index]+dp(index+1,remw-w[index]);
-		int  leave=dp(index+1,remw);
-		return dp[index][remw]=Math.max(take,leave);
-		
-	}
-	public static void print(int []x) {
-		for(int a:x) {
-			System.out.print(a+" ");
 		}
-	}
-	public static void main (String []args) throws IOException {
 		
-		Scanner sc=new Scanner (System.in);
-		PrintWriter pw=new PrintWriter(System.out);
-		
-		n=sc.nextInt();
-		s=sc.nextInt();
-		w=new int[n];
-		v=new int[n];
-		for(int i=0;i<n;i++) {
-			w[i]=sc.nextInt();
-			v[i]=sc.nextInt();
+		if(index==-1) {
+			return 0;
 			
 		}
 		
-//		print(w);
-//		System.out.println();
-//		print(v);
-//		v=sc.nextIntArr(n);
-//		w=sc.nextIntArr(n);
-		
-
-
-
-
-		dp=new int [n+1][s+1];
-		for(int[]x:dp) {
-			Arrays.fill(x,-1);
+		if(dp[index][w]!=-1) {
+			return dp[index][w];
 		}
-//		for (int i = n-1;i>=0;i-=1000 ) {
-//			for(int j=0;j<s+1;j+=1000) {
-//				dp(i,j);
-//			}
-//		}
-		pw.println(dp(0,s));
-		pw.close();
+		
+		
+		int  x=weights[index];
+		long take=values[index]+knapsack(index-1,(w-x));
+		long leave=knapsack(index-1,w);
+		
+		return dp[index][w]=Math.max(take,leave);
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner (System.in);
+		n =sc.nextInt();
+		w=sc.nextInt();
+		dp=new long [n+1][w+1];
+		for(long m[]:dp) {
+			Arrays.fill(m,-1);
+		}
+		
+		weights=new int [n];
+		values=new int [n];
+		for (int i = 0; i < n; i++) {
+			weights[i]=sc.nextInt();
+			values[i]=sc.nextInt();
+					
+					
+			
+		}
+		
+		long result=knapsack(n-1,w);
+		System.out.println(result);
+		
+		
 		
 
 	}
+	
 	static class Scanner {
 		BufferedReader br;
 		StringTokenizer st;
@@ -95,120 +92,5 @@ public class Knapsack {
 			return arr;
 		}
 	}
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	static int[] w, v;
-//	static int n, s;
-//	static int[][] dp;
-//	static StringBuilder sb;
-//	public static int dp(int idx, int remw) {
-//		if (remw < 0) {
-//			return -(int) 1e9;
-//		}
-//		if (idx == n) {
-//			return 0;
-//		}
-//		if (dp[idx][remw] != -1) {
-//			return dp[idx][remw];
-//		}
-//		int take = v[idx] + dp(idx + 1, remw - w[idx]);
-//		int leave = dp(idx + 1, remw);
-//		return dp[idx][remw] = Math.max(take, leave);
-//	}
-//	public static void trace(int idx, int remw) {
-//		if (idx == n)
-//			return;
-//		if (dp(idx, remw) == dp(idx + 1, remw)) {
-//			trace(idx + 1, remw);
-//		} else {
-//			sb.append(idx + " ");
-//			trace(idx + 1, remw - w[idx]);
-//		}
-//	}
-//	public static void traceIte(int idx, int remw) {
-//		while (idx != n) {
-//			if (dp(idx, remw) == dp(idx + 1, remw)) {
-//				idx++;
-//			} else {
-//				sb.append(idx + " ");
-//				idx++;
-//				remw -= w[idx];
-//			}
-//		}
-//	}
-//	public static void main(String[] args) throws IOException {
-//		Scanner sc = new Scanner(System.in);
-//		PrintWriter pw = new PrintWriter(System.out);
-//		n = sc.nextInt();
-//		s = sc.nextInt();
-//		v = sc.nextIntArr(n);
-//		w = sc.nextIntArr(n);
-//		dp = new int[n + 1][s + 1];
-//		for (int[] x : dp)
-//			Arrays.fill(x, -1);
-//		for (int i = n - 1; i >= 0; i-=1000) {
-//			for (int j = 0; j < s + 1; j+=1000)
-//				dp(i, j);
-//		}
-//		pw.println(dp(0, s));
-//		sb = new StringBuilder();
-//		traceIte(0, s);
-//		pw.println(sb);
-//		for (int idx = n - 1; idx >= 0; idx--) {
-//			for (int remw = 0; remw < s + 1; remw++) {
-//				int leave = dp[idx + 1][remw];
-//				int take = (remw - w[idx]) < 0 ? -(int) 1e9 : (v[idx] + dp[idx + 1][remw - w[idx]]);
-//				dp[idx][remw] = Math.max(leave, take);
-//			}
-//		}
-//		for (int idx = n - 1; idx >= 0; idx--) {
-//			for (int remw = 0; remw < s + 1; remw++) {
-//				int leave = dp[1][remw];
-//				int take = (remw - w[idx]) < 0 ? -(int) 1e9 : (v[idx] + dp[1][remw - w[idx]]);
-//				dp[0][remw] = Math.max(leave, take);
-//			}
-//			dp[1] = dp[0];
-//			dp[0] = new int[s + 1];
-//		}
-		// optional trick
-//		int[] arr = new int[s + 1];
-//		for (int idx = n - 1; idx >= 0; idx--) {
-//			for (int remw = s; remw >= 0; remw--) {
-//				arr[remw] = Math.max(arr[remw], (remw - w[idx]) < 0 ? -(int) 1e9 : (v[idx] + arr[remw - w[idx]]));
-//			}
-//		}
-//		pw.println(dp[0][s]);
-//		pw.close();
-//	}
 
+}
